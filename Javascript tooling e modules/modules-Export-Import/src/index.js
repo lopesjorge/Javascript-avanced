c3bb7367b37bc16f718bba9a06fc02880abd3639
lopesjorge <jorgelopeslopes@outlook.com>
2023-11-22 21:27:56 -0300

    create module webp boirplate

---
+++ Javascript tooling e modules/modules-Export-Import/src/index.js
@@ -0,0 +1,9 @@
+// 1 rodar esse comando para funcionar no node 20 export NODE_OPTIONS=--openssl-legacy-provider
+// 2
+// 3
+import module1 from './modulo1' //Importando arquivos
+import module2 from './modulo2' //Importando arquivos
+
+
+module1()
+module2()
---
+++ Javascript tooling e modules/modules-Export-Import/src/modulo1.js
@@ -0,0 +1,4 @@
+export default function module1(){
+    console.log('export');
+    alert('Exportando modulo 1')
+}
